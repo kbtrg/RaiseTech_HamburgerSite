9042693674162382e693c6e0e2f8f3f6205f4ecd
Kubori <kbtrg.527@gmail.com>
2021-08-30 15:59:42 +0900

    front-page--pc fin

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
+//ハンバーガーボタン処理
 $(function(){
     $('.ham-menu').click(function() {
-        $('.l-sidebar').fadeIn(300);  
+        $('.l-sidebar').fadeIn(250);
+        $('.left-block').css('position', 'fixed');
     });
     $('.c-ham-btn').click(function() {
-        $('.l-sidebar').fadeOut(300);  
+        $('.l-sidebar').fadeOut(250);  
+        $('.left-block').css('position', 'static');
     });
 });
