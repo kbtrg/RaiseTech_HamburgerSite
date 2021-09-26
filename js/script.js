1877ce20b48f34410653c24a8d587ec8cf90bf69
Kubori <kbtrg.527@gmail.com>
2021-09-26 11:51:50 +0900

    top-page responsive start

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 $(function(){
     $('.ham-menu').click(function() {
         $('.l-sidebar').fadeIn(250);
-        $('.left-block').css('position', 'fixed');
+        $('.left-block').css({position: "fixed", width: "100%"});
     });
     $('.c-ham-btn').click(function() {
         $('.l-sidebar').fadeOut(250);  
