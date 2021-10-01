d1952c4996a2576990f10d94c78d3b915d7902ad
Kubori <kbtrg.527@gmail.com>
2021-10-01 09:36:23 +0900

    responsive header fin. but sidebar yet.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,56 @@
-//ハンバーガーボタン処理
-$(function(){
-    $('.ham-menu').click(function() {
-        $('.l-sidebar').fadeIn(250);
-        $('.left-block').css({position: "fixed", width: "100%"});
-    });
-    $('.c-ham-btn').click(function() {
-        $('.l-sidebar').fadeOut(250);  
-        $('.left-block').css('position', 'static');
-    });
+/* 共通処理
+-----------------------------------------------------------------*/
+//リサイズ時に読み込み
+$(window).resize(function(){
+    $('.l-sidebar').fadeOut(250);  
+    $('.left-block').css('position', 'static');
 });
+
+
+
+
+/* Windowサイズに合わせた処理
+-----------------------------------------------------------------*/
+//Windowサイズの取得
+$windowWidth = window.innerWidth;
+//Windowサイズの定義
+$sp = 787;
+$iPad = 980;
+//Windowサイズ判定
+isMobileSize = ($windowWidth < $sp);
+isTabletSize = ($windowWidth <= $iPad) && ($windowWidth > $sp);
+isPcSize = ($windowWidth > $iPad);
+
+
+//モバイルサイズの場合の記述
+if(isMobileSize){
+    //ハンバーガーボタン処理
+    $(function(){
+        $('.ham-menu').click(function() {
+            $('.l-sidebar').fadeIn(250);
+            $('.left-block').css({position: "fixed", width: "100%"});
+        });
+        $('.c-ham-btn').click(function() {
+            $('.l-sidebar').fadeOut(250);  
+            $('.left-block').css('position', 'static');
+        });
+    });
+}
+//タブレットサイズの場合の記述
+else if(isTabletSize){
+    //ハンバーガーボタン処理
+    $(function(){
+        $('.ham-menu').click(function() {
+            $('.l-sidebar').fadeIn(250);
+            $('.left-block').css({position: "fixed", width: "100%"});
+        });
+        $('.c-ham-btn').click(function() {
+            $('.l-sidebar').fadeOut(250);  
+            $('.left-block').css('position', 'static');
+        });
+    });
+}
+//PCサイズの場合の記述
+else if(isPcSize){
+    
+}
